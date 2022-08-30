15cccbcbbe3a0c70fa76196fd75ea53ccfc6067c
saralaloux <sara.laloux@gmail.com>
2022-08-30 09:21:08 +0200

    add

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   
-<Router>
+<Router basename='/sara-portfolio'>
   <App />
 </Router>
 );
